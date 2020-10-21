6894cc6bcc2c4aa21ea424b4dd388ea215efe989
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2020-10-22 00:58:07 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,3 +1,3 @@
 WORKSPACE=/home/appy/jenkins-data/jenkins_home/workspace/My-Project
 
-sudo docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
