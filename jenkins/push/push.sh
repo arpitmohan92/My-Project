934d8b8c3f19ed6708d4267e3b4b6f861cc2044b
root <root@ip-172-31-2-71.us-west-2.compute.internal>
2020-10-22 06:57:49 +0000

    Revert "done"
    This reverts commit 2c686304df2324d69ac21b4866bb75c8fffd40c8.

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -9,9 +9,9 @@ IMAGE="app"
 echo "** Logging in ***"
 docker login -u amsmzn -p Amsmzn@123
 echo "*** Tagging image ***"
-docker tag $IMAGE:$TAG amsmzn/$IMAGE:$TAG
+docker tag $IMAGE:$BUILD_TAG amsmzn/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
-docker push amsmzn/$IMAGE:$TAG
+docker push amsmzn/$IMAGE:$BUILD_TAG
 echo "*** Deleting previous image before tagging ***"
-docker rmi $IMAGE:$TAG
+docker rmi $IMAGE:$BUILD_TAG
 
