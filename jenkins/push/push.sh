2c686304df2324d69ac21b4866bb75c8fffd40c8
root <root@ip-172-31-2-71.us-west-2.compute.internal>
2020-10-22 06:44:10 +0000

    done

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -9,9 +9,9 @@ IMAGE="app"
 echo "** Logging in ***"
 docker login -u amsmzn -p Amsmzn@123
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG amsmzn/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$TAG amsmzn/$IMAGE:$TAG
 echo "*** Pushing image ***"
-docker push amsmzn/$IMAGE:$BUILD_TAG
+docker push amsmzn/$IMAGE:$TAG
 echo "*** Deleting previous image before tagging ***"
-docker rmi $IMAGE:$BUILD_TAG
+docker rmi $IMAGE:$TAG
 
