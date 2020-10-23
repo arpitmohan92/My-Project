55f87b1df0408a958cee0f50303a05e3ed4d68b9
root <root@ip-172-31-2-71.us-west-2.compute.internal>
2020-10-23 19:07:13 +0000

    done

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,3 +1,5 @@
+rm -rf *.war
+
 WORKSPACE=/home/appy/jenkins-data/jenkins_home/workspace/My-Project
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
