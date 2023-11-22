baef575e69f10aa067b5cf6f11936a4355e8f57b
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-11-22 13:29:12 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -16,15 +16,20 @@ for lb_info in $(echo "$load_balancers" | jq -c '.[]'); do
 
     # Loop through each target group and check target health
     for target_group_arn in $(echo "$target_group_arns" | jq -r '.[]'); do
-        target_count=$(aws elbv2 describe-target-health --target-group-arn $target_group_arn --query 'TargetHealthDescriptions[?TargetHealth.State==`healthy`].length(@)' --output json --profile common-dev)
+        # Validate that the string appears to be a valid ARN
+        if [[ "$target_group_arn" =~ ^arn:aws:elasticloadbalancing:[^:]+:targetgroup/[^/]+$ ]]; then
+            target_count=$(aws elbv2 describe-target-health --target-group-arn $target_group_arn --query 'TargetHealthDescriptions[?TargetHealth.State==`healthy`].length(@)' --output json --profile common-dev)
 
-        # Check if target_count is an integer
-        if [[ "$target_count" =~ ^[0-9]+$ ]]; then
-            if [ "$target_count" -eq 0 ]; then
-                echo "$lb_name" >> "$output_file"
+            # Check if target_count is an integer
+            if [[ "$target_count" =~ ^[0-9]+$ ]]; then
+                if [ "$target_count" -eq 0 ]; then
+                    echo "$lb_name" >> "$output_file"
+                fi
+            else
+                echo "Error: Unable to retrieve target count for Load Balancer '$lb_name' with ARN $lb_arn and Target Group ARN $target_group_arn." >&2
             fi
         else
-            echo "Error: Unable to retrieve target count for Load Balancer '$lb_name' with ARN $lb_arn and Target Group ARN $target_group_arn." >&2
+            echo "Error: Invalid target group ARN format: $target_group_arn for Load Balancer '$lb_name' with ARN $lb_arn." >&2
         fi
     done
 done
