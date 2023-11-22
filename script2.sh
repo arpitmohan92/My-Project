abc416dd47254ca2843bed5d343be095bc49dcb6
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-11-22 14:45:31 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -6,7 +6,7 @@ output_file="zero_target_lb_names.txt"
 # Get a list of load balancer ARNs and names
 load_balancers=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[*].[LoadBalancerArn,LoadBalancerName]' --output json --profile common-dev)
 
-# Loop through each load balancer and check if any target group has zero target instances
+# Loop through each load balancer and check if none of the target groups have healthy targets
 while IFS= read -r lb_info; do
     lb_arn=$(echo "$lb_info" | jq -r '.[0]')
     lb_name=$(echo "$lb_info" | jq -r '.[1]')
@@ -14,8 +14,8 @@ while IFS= read -r lb_info; do
     # Get the target group ARNs for the load balancer
     target_group_arns=$(aws elbv2 describe-target-groups --load-balancer-arn "$lb_arn" --query 'TargetGroups[*].TargetGroupArn' --output json --profile common-dev)
 
-    # Flag to check if any target group has zero healthy targets
-    has_zero_targets=false
+    # Flag to check if any target group has healthy targets
+    any_target_group_has_healthy_targets=false
 
     # Loop through each target group and check target health
     while IFS= read -r target_group_arn; do
@@ -23,19 +23,19 @@ while IFS= read -r lb_info; do
 
         # Check if target_count is an integer
         if [[ "$target_count" =~ ^[0-9]+$ ]]; then
-            if [ "$target_count" -eq 0 ]; then
-                has_zero_targets=true
-                break  # Exit the loop if any target group has zero healthy targets
+            if [ "$target_count" -ne 0 ]; then
+                any_target_group_has_healthy_targets=true
+                break  # Exit the loop if any target group has healthy targets
             fi
         else
             echo "Error: Unable to retrieve target count for Load Balancer '$lb_name' with ARN $lb_arn and Target Group ARN $target_group_arn." >&2
         fi
     done <<< "$(echo "$target_group_arns" | jq -c -r '.[]')"
 
-    # If any target group has zero healthy targets, record the load balancer name
-    if [ "$has_zero_targets" = true ]; then
+    # If none of the target groups have healthy targets, record the load balancer name
+    if [ "$any_target_group_has_healthy_targets" = false ]; then
         echo "$lb_name" >> "$output_file"
     fi
 done <<< "$(echo "$load_balancers" | jq -c '.[]')"
 
-echo "Load balancer names with zero target instances have been written to $output_file"
+echo "Load balancer names with none of the target groups having healthy targets have been written to $output_file"
