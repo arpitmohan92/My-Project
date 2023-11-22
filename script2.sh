593b2f0db4ade464e6169cc53684e3b806b88e22
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-11-22 14:38:37 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -6,8 +6,8 @@ output_file="zero_target_lb_names.txt"
 # Get a list of load balancer ARNs and names
 load_balancers=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[*].[LoadBalancerArn,LoadBalancerName]' --output json --profile common-dev)
 
-# Loop through each load balancer and check if it has any target groups with zero target instances
-for lb_info in $(echo "$load_balancers" | jq -c '.[]'); do
+# Loop through each load balancer and check if any target group has zero target instances
+while IFS= read -r lb_info; do
     lb_arn=$(echo "$lb_info" | jq -r '.[0]')
     lb_name=$(echo "$lb_info" | jq -r '.[1]')
 
@@ -18,23 +18,24 @@ for lb_info in $(echo "$load_balancers" | jq -c '.[]'); do
     has_zero_targets=false
 
     # Loop through each target group and check target health
-    for target_group_arn in $(echo "$target_group_arns" | jq -r '.[]'); do
+    while IFS= read -r target_group_arn; do
         target_count=$(aws elbv2 describe-target-health --target-group-arn "$target_group_arn" --query 'TargetHealthDescriptions[?TargetHealth.State==`healthy`].length(@)' --output json --profile common-dev)
 
         # Check if target_count is an integer
         if [[ "$target_count" =~ ^[0-9]+$ ]]; then
             if [ "$target_count" -eq 0 ]; then
                 has_zero_targets=true
+                break  # Exit the loop if any target group has zero healthy targets
             fi
         else
             echo "Error: Unable to retrieve target count for Load Balancer '$lb_name' with ARN $lb_arn and Target Group ARN $target_group_arn." >&2
         fi
-    done
+    done <<< "$(echo "$target_group_arns" | jq -c -r '.[]')"
 
     # If any target group has zero healthy targets, record the load balancer name
     if [ "$has_zero_targets" = true ]; then
         echo "$lb_name" >> "$output_file"
     fi
-done
+done <<< "$(echo "$load_balancers" | jq -c '.[]')"
 
 echo "Load balancer names with zero target instances have been written to $output_file"
