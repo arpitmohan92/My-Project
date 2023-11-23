24b750b963ea3d07c0d29903e71e4bcdf598bd36
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-11-23 12:02:50 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -6,24 +6,24 @@ AWS_REGION="your-region"
 # Specify your AWS CLI profile
 AWS_PROFILE="common-dev"
 
-# Get a list of all security group IDs
-all_security_groups=$(aws ec2 describe-security-groups --region $AWS_REGION --profile $AWS_PROFILE --query 'SecurityGroups[*].GroupId' --output text)
+# Get a list of all RDS instances
+all_rds_instances=$(aws rds describe-db-instances --region $AWS_REGION --profile $AWS_PROFILE --query 'DBInstances[*].[DBInstanceIdentifier, DBClusterIdentifier, CurrentConnections]' --output table)
 
-# Initialize an array to store unattached security groups
-unattached_security_groups=()
+# Initialize an array to store RDS instances with zero connections
+zero_connections_rds=()
 
-# Loop through each security group
-for sg_id in $all_security_groups; do
-    # Check if the security group is attached to any resource
-    attached_resources=$(aws ec2 describe-instances --region $AWS_REGION --profile $AWS_PROFILE --filters Name=instance.group-id,Values=$sg_id --query 'Reservations[*].Instances[*].InstanceId' --output text)
-    
-    # If not attached, add to the unattached_security_groups array
-    if [ -z "$attached_resources" ]; then
-        unattached_security_groups+=("$sg_id")
+# Loop through each RDS instance
+while read -r rds_instance; do
+    # Extract the number of current connections
+    current_connections=$(echo $rds_instance | awk '{print $NF}')
+
+    # Check if the number of connections is zero
+    if [ "$current_connections" -eq 0 ]; then
+        zero_connections_rds+=("$rds_instance")
     fi
-done
+done <<< "$all_rds_instances"
 
-# Save the unattached security groups to a file
-echo "${unattached_security_groups[@]}" > unattached_security_groups.txt
+# Save the RDS instances with zero connections to a file
+echo "${zero_connections_rds[@]}" > zero_connections_rds.txt
 
-echo "Unattached security groups have been recorded in unattached_security_groups.txt"
+echo "RDS instances with zero connections have been recorded in zero_connections_rds.txt"
