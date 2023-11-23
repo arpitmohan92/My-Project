a063bbc8d9b978957257f5a4ba3cd0ae8d6339cc
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-11-23 09:31:18 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Output file for ASG names with running instances
-output_file="running_instances_asg.txt"
+# Output file for ASG names with missing instances
+output_file="asg_missing_instances.txt"
 
 # Get a list of Auto Scaling Groups and their instances
 asg_instances=$(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName,Instances[*].InstanceId]' --output json)
 
-# Loop through each ASG and check if it has running instances
-echo "$asg_instances" | jq -c -r '.[] | select(.[1] != null) | .[0]' > "$output_file"
+# Loop through each ASG and check if instance IDs are missing
+echo "$asg_instances" | jq -c -r '.[] | select(.[1] == null) | .[0]' > "$output_file"
 
-echo "Auto Scaling Group names with running instances have been written to $output_file"
+echo "Auto Scaling Group names with missing instances have been written to $output_file"
