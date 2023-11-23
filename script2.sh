71a8f64eead255c9690e557b09ee758a4b2b844f
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-11-23 10:28:57 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-# Output file for ASG names with missing instances
-output_file="asg_missing_instances.txt"
+# Specify your AWS region
+AWS_REGION="your-region"
 
-# Get a list of Auto Scaling Groups and their instances
-asg_instances=$(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName,Instances[*].InstanceId]' --output json)
+# Specify your AWS CLI profile
+AWS_PROFILE="common-dev"
 
-# Loop through each ASG and check if instance IDs are missing
-echo "$asg_instances" | jq -c -r '.[] | select(.[1] == null) | .[0]' > "$output_file"
+# Get a list of all security group IDs
+all_security_groups=$(aws ec2 describe-security-groups --region $AWS_REGION --profile $AWS_PROFILE --query 'SecurityGroups[*].GroupId' --output text)
 
-echo "Auto Scaling Group names with missing instances have been written to $output_file"
+# Initialize an array to store unattached security groups
+unattached_security_groups=()
+
+# Loop through each security group
+for sg_id in $all_security_groups; do
+    # Check if the security group is attached to any resource
+    attached_resources=$(aws ec2 describe-instances --region $AWS_REGION --profile $AWS_PROFILE --filters Name=instance.group-id,Values=$sg_id --query 'Reservations[*].Instances[*].InstanceId' --output text)
+    
+    # If not attached, add to the unattached_security_groups array
+    if [ -z "$attached_resources" ]; then
+        unattached_security_groups+=("$sg_id")
+    fi
+done
+
+# Save the unattached security groups to a file
+echo "${unattached_security_groups[@]}" > unattached_security_groups.txt
+
+echo "Unattached security groups have been recorded in unattached_security_groups.txt"
