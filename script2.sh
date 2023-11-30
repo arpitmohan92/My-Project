a84eabe53453524c5e273a1f01550473ec027ceb
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-11-30 15:50:12 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+Presentation Tier (Web Tier):
+This tier is responsible for interacting with users and presenting the user interface.
+It often involves a web server, which could be an EC2 instance running a web server software like Apache or Nginx.
+In a VPC diagram, this tier might reside in a public subnet, allowing direct access from the internet.
+Application Tier (Logic Tier):
+The application tier processes business logic and executes the core functionality of the application.
+It can include multiple servers or instances running application code, which could be hosted on EC2 instances or within containers managed by AWS ECS (Elastic Container Service).
+In a VPC diagram, this tier might reside in a private subnet, limiting direct external access and enhancing security.
+Data Tier (Database Tier):
+The data tier stores and manages the application's data.
+It often involves a database server, which could be an Amazon RDS instance for a relational database or an Amazon DynamoDB table for a NoSQL database.
+In a VPC diagram, this tier might also reside in a private subnet to restrict direct access and enhance security.
+VPC Configuration:
 
-# Get a list of all Elastic IPs
-all_ips=$(aws ec2 describe-addresses --query 'Addresses[*].PublicIp' --output text)
-
-# Iterate through each Elastic IP
-for ip in $all_ips; do
-    # Check if the Elastic IP is associated with any resource
-    association=$(aws ec2 describe-addresses --public-ips $ip --query 'Addresses[*].AssociationId' --output text)
-    
-    if [ -z "$association" ]; then
-        echo "Unattached Elastic IP: $ip"
-    fi
-done
+The entire architecture operates within an AWS Virtual Private Cloud (VPC), providing network isolation and control.
+Subnets are used to organize resources. Public subnets may contain resources that need direct internet access, while private subnets house resources that should not be directly accessible from the internet.
+Security Groups and Network ACLs are configured to control inbound and outbound traffic between different tiers. For example, only allowing necessary traffic from the web tier to the application tier.
