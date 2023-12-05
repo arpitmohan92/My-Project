e268aa0967682b500eeb7fade6f5fb51f8b83654
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-12-05 13:24:25 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,52 +1,20 @@
-provider "aws" {
-  region = "your-region"
-}
+import boto3
 
-# Create an ECS Task Restart Lambda Function
-resource "aws_lambda_function" "ecs_restart_lambda" {
-  filename      = "ecs_restart.zip"  # Path to your Lambda deployment package
-  function_name = "ecsRestartFunction"
-  handler       = "ecs_restart.sh"   # Name of your script
-  runtime       = "provided"
-  role          = aws_iam_role.lambda_execution_role.arn
-}
+def lambda_handler(event, context):
+    # Create ECS client
+    ecs_client = boto3.client('ecs')
 
-# Create an IAM role for Lambda execution
-resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
+    # List ECS clusters
+    response = ecs_client.list_clusters()
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-# Attach necessary policies to Lambda execution role
-resource "aws_iam_role_policy_attachment" "lambda_execution_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.lambda_execution_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
-  role       = aws_iam_role.lambda_execution_role.name
-}
-
-# Create CloudWatch Events Rule
-resource "aws_cloudwatch_event_rule" "ecs_restart_rule" {
-  name                = "ECSRestartRule"
-  schedule_expression = "cron(0 0 * * ? *)"  # Example: daily at midnight UTC
-}
-
-# Add Lambda function as a target for the CloudWatch Events Rule
-resource "aws_cloudwatch_event_target" "ecs_restart_target" {
-  rule      = aws_cloudwatch_event_rule.ecs_restart_rule.name
-  target_id = "ecsRestartTarget"
-  arn       = aws_lambda_function.ecs_restart_lambda.arn
-}
+    if 'clusterArns' in response and response['clusterArns']:
+        clusters = response['clusterArns']
+        return {
+            'statusCode': 200,
+            'body': f'ECS Clusters: {clusters}'
+        }
+    else:
+        return {
+            'statusCode': 404,
+            'body': 'No ECS clusters found.'
+        }
