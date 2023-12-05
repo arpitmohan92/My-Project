0ee2efb96f92b8a5b6b0b2a0fa191ff88eee204c
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-12-05 16:28:31 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,31 +1,9 @@
 import boto3
 
-def lambda_handler(event, context):
-    # Create EC2 client
-    ec2_client = boto3.client('ec2')
+ecs_client = boto3.client('ecs')
 
-    # List EC2 instances
-    response = ec2_client.describe_instances()
-
-    instances = []
-    
-    # Extract instance information from the response
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            instances.append({
-                'InstanceId': instance['InstanceId'],
-                'InstanceType': instance['InstanceType'],
-                'State': instance['State']['Name']
-            })
-
-    if instances:
-        return {
-            'statusCode': 200,
-            'body': f'EC2 Instances: {instances}'
-        }
-    else:
-        return {
-            'statusCode': 404,
-            'body': 'No EC2 instances found.'
-        }
-
+lambda_handler = lambda event, context: ecs_client.update_service(
+    cluster='dev',
+    service='apps-checkin',
+    forceNewDeployment=True
+)
