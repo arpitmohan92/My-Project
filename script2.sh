9d602d4eb7e0588b363200a5560a3e6ae93f88dc
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-12-05 16:53:28 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,5 +1,4 @@
 import boto3
-import json
 from datetime import datetime
 
 ecs_client = boto3.client('ecs')
@@ -14,9 +13,9 @@ def lambda_handler(event, context):
 
         # Convert datetime object to string before returning
         response['timestamp'] = str(datetime.now())
-        
-        # Serialize the response using json.dumps
-        response_serializable = json.dumps(response, default=str)
+
+        # Serialize the response using boto3's json_util
+        response_serializable = ecs_client.meta.events._json_handler(response, None)
         
         return response_serializable
 
