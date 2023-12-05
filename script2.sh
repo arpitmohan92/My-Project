3c5ae3f9b4cd81d53c0b1219b34d7d5d3e1e4b27
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-12-05 16:39:00 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -4,13 +4,20 @@ from datetime import datetime
 ecs_client = boto3.client('ecs')
 
 def lambda_handler(event, context):
-    response = ecs_client.update_service(
-        cluster='dev',
-        service='apps-checkin',
-        forceNewDeployment=True
-    )
+    try:
+        response = ecs_client.update_service(
+            cluster='dev',
+            service='apps-checkin',
+            forceNewDeployment=True
+        )
 
-    # Convert datetime object to string before returning
-    response['timestamp'] = str(datetime.now())
+        # Convert datetime object to string before returning
+        response['timestamp'] = str(datetime.now())
 
-    return response
+        # Ensure all elements are serializable
+        response_serializable = json.dumps(response, default=str)
+        
+        return response_serializable
+
+    except Exception as e:
+        return str(e)
