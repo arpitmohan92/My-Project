994f1124f3c802cebfff1c83a6506b8ccf872e51
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-12-06 11:46:46 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,33 +1,15 @@
-import boto3
-import json
-from datetime import datetime
-
-ecs_client = boto3.client('ecs')
-
-def lambda_handler(event, context):
-    try:
-        # Update ECS service
-        ecs_client.update_service(
-            cluster='dev',
-            service='apps-checkin',
-            forceNewDeployment=True
-        )
-
-        # Get current timestamp
-        current_time = datetime.now()
-        current_time_str = current_time.strftime('%Y-%m-%d %H:%M:%S')
-
-        # Return JSON response
-        response = {
-            'statusCode': 200,
-            'body': json.dumps({'message': 'Service updated successfully', 'timestamp': current_time_str})
-        }
-
-        return response
-    except Exception as e:
-        # Handle exceptions appropriately
-        error_response = {
-            'statusCode': 500,
-            'body': json.dumps({'error_message': str(e)})
-        }
-        return error_response
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:RegisterTaskDefinition",
+        "ecs:UpdateService",
+        "ecs:UpdateTaskSet",
+        "ecs:UpdateCluster"
+      ],
+      "Resource": "arn:aws:ecs:region:account-id:cluster/your-ecs-cluster-name"
+    }
+  ]
+}
