ec9526cc33a24235c59fede462f8eaffef890ee3
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-12-15 14:29:15 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,15 +1,14 @@
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ecs:RegisterTaskDefinition",
-        "ecs:UpdateService",
-        "ecs:UpdateTaskSet",
-        "ecs:UpdateCluster"
-      ],
-      "Resource": "arn:aws:ecs:region:account-id:cluster/your-ecs-cluster-name"
-    }
-  ]
-}
+#!/bin/bash
+
+# Get a list of all Lambda functions
+lambda_functions=$(aws lambda list-functions --query 'Functions[*].FunctionName' --output text)
+
+# Loop through each function and check if it has any event source mappings
+for function_name in $lambda_functions; do
+    event_sources=$(aws lambda list-event-source-mappings --function-name $function_name --query 'EventSourceMappings' --output text)
+    
+    # If the function has no event sources, consider it as potentially unused
+    if [ -z "$event_sources" ]; then
+        echo "Unused Lambda function: $function_name"
+    fi
+done
