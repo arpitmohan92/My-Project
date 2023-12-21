e18155719fc76dbb9e57f917c4a4b27ccc25975d
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2023-12-21 11:27:20 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,49 +1,33 @@
-#!/bin/bash
+import boto3
+from datetime import datetime, timedelta
 
-# Set your AWS CLI profile and region
-AWS_PROFILE="your_aws_profile"
-AWS_REGION="your_aws_region"
-
-# Get VPC ID
-VPC_ID="your_vpc_id"
-
-# Check if VPC exists
-aws ec2 describe-vpcs --vpc-ids "$VPC_ID" --profile "$AWS_PROFILE" --region "$AWS_REGION" > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "VPC with ID $VPC_ID does not exist or there was an issue retrieving information."
-    exit 1
-fi
-
-# Check Subnet usage
-SUBNETS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" --profile "$AWS_PROFILE" --region "$AWS_REGION" --output json | jq -r '.Subnets | length')
-if [ "$SUBNETS" -eq 0 ]; then
-    echo "No subnets found for VPC $VPC_ID."
-else
-    echo "Subnets found: $SUBNETS"
-fi
-
-# Check Security Group usage
-SECURITY_GROUPS=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$VPC_ID" --profile "$AWS_PROFILE" --region "$AWS_REGION" --output json | jq -r '.SecurityGroups | length')
-if [ "$SECURITY_GROUPS" -eq 0 ]; then
-    echo "No security groups found for VPC $VPC_ID."
-else
-    echo "Security groups found: $SECURITY_GROUPS"
-fi
-
-# Check Route Table usage
-ROUTE_TABLES=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$VPC_ID" --profile "$AWS_PROFILE" --region "$AWS_REGION" --output json | jq -r '.RouteTables | length')
-if [ "$ROUTE_TABLES" -eq 0 ]; then
-    echo "No route tables found for VPC $VPC_ID."
-else
-    echo "Route tables found: $ROUTE_TABLES"
-fi
-
-# Check Network ACL usage
-NETWORK_ACLS=$(aws ec2 describe-network-acls --filters "Name=vpc-id,Values=$VPC_ID" --profile "$AWS_PROFILE" --region "$AWS_REGION" --output json | jq -r '.NetworkAcls | length')
-if [ "$NETWORK_ACLS" -eq 0 ]; then
-    echo "No network ACLs found for VPC $VPC_ID."
-else
-    echo "Network ACLs found: $NETWORK_ACLS"
-fi
-
-echo "Script execution completed."
+def lambda_handler(event, context):
+    # Set the AWS region
+    region = 'your_region'
+    
+    # Create EC2 client
+    ec2 = boto3.client('ec2', region_name=region)
+    
+    # Get the current date
+    current_date = datetime.utcnow()
+    
+    # Define the retention period (in days)
+    retention_days = 30
+    
+    # Calculate the cutoff date for deletion
+    cutoff_date = current_date - timedelta(days=retention_days)
+    
+    # Describe all snapshots in the region
+    response = ec2.describe_snapshots(OwnerIds=['self'])
+    
+    # Loop through each snapshot and delete if older than cutoff_date
+    deleted_snapshots = []
+    for snapshot in response['Snapshots']:
+        snapshot_date = snapshot['StartTime'].replace(tzinfo=None)
+        if snapshot_date < cutoff_date:
+            # Delete the snapshot
+            ec2.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+            deleted_snapshots.append(snapshot['SnapshotId'])
+    
+    # Print the IDs of the deleted snapshots
+    print("Deleted Snapshots: {}".format(deleted_snapshots))
