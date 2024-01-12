ce952579a8257e8bc29b16618bbd22151a8eb691
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-12 16:55:30 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,33 +1,8 @@
-import boto3
-
-def lambda_handler(event, context):
-    security_hub = boto3.client('securityhub')
-
-    # Specify the finding types you want to suppress (e.g., LOW)
-    finding_types_to_suppress = ['Low']
-
-    # Specify the reason for suppression
-    suppression_reason = 'Suppressing low alerts for maintenance window.'
-
-    for finding_type in finding_types_to_suppress:
-        response = security_hub.batch_update_findings(
-            FindingIdentifiers=[
-                {
-                    'Id': finding['Id'],
-                    'ProductArn': finding['ProductArn']
-                }
-                for finding in event['findings']
-                if finding['Severity']['Label'] == finding_type
-            ],
-            Workflow=[
-                {
-                    'Status': 'SUPPRESSED',
-                    'StatusReason': suppression_reason
-                }
-            ]
-        )
-
-    return {
-        'statusCode': 200,
-        'body': 'Low alerts suppressed successfully.'
-    }
+{
+  "errorMessage": "'findings'",
+  "errorType": "KeyError",
+  "requestId": "b49af8d3-582a-4f0b-afbe-e0a551124a75",
+  "stackTrace": [
+    "  File \"/var/task/lambda_function.py\", line 19, in lambda_handler\n    for finding in event['findings']\n"
+  ]
+}
