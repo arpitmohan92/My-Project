e7b7e29b3abe40dd567df155a6cbad9860ccd256
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-12 12:52:35 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,8 +1,34 @@
-{
-  "errorMessage": "'detail'",
-  "errorType": "KeyError",
-  "requestId": "853dc23e-63fc-4b07-9e54-8353bc4600a3",
-  "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 6, in lambda_handler\n    for finding in event['detail']['findings']:\n"
-  ]
-}
+import boto3
+import logging
+
+def lambda_handler(event, context):
+    logging.getLogger().setLevel(logging.INFO)  # Enable logging
+
+    try:
+        securityhub = boto3.client('securityhub')
+
+        findings = event.get('detail', {}).get('findings')
+        if findings:
+            findingIds = [finding['Id'] for finding in findings]
+
+            securityhub.batch_update_findings(
+                FindingIdentifiers=findingIds,
+                Workflow={'Status': 'SUPPRESSED'}
+            )
+
+            return {
+                'statusCode': 200,
+                'body': 'Low-severity findings suppressed.'
+            }
+        else:
+            logging.warning("Event does not contain 'detail' or 'findings' keys")
+            return {
+                'statusCode': 400,
+                'body': 'Invalid event structure'
+            }
+    except Exception as e:
+        logging.error(f"Error suppressing findings: {e}")
+        return {
+            'statusCode': 500,
+            'body': 'Internal server error'
+        }
