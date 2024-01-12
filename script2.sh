7064d9a6986034c63097dcd354f13728c1903a3b
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-12 16:34:22 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,29 +1,6 @@
-import boto3
 
-securityhub = boto3.client('securityhub')
-
-suppress_alerts = lambda event, context: securityhub.batch_update_findings(
-    FindingIdentifiers=[
-        {
-            'Id': finding['Id'],
-            'ProductArn': finding['ProductArn']
-        } for finding in securityhub.get_findings(
-            Filters={
-                'SeverityLabel': {
-                    'Comparison': 'EQUALS',
-                    'Value': 'LOW'
-                },
-                'ConfidenceLabel': {
-                    'Comparison': 'EQUALS',
-                    'Value': 'LOW'
-                },
-                'WorkflowState': {
-                    'Comparison': 'NOT_EQUALS',
-                    'Value': 'SUPPRESSED'
-                }
-            },
-            MaxResults=10
-        )['Findings']
-    ],
-    Workflow='SUPPRESS'
-)
+  "errorMessage": "Handler 'lambda_handler' missing on module 'lambda_function'",
+  "errorType": "Runtime.HandlerNotFound",
+  "requestId": "97b225c4-76e3-4571-8cbd-8978e228a3b6",
+  "stackTrace": []
+}
