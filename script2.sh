068eca57d5292f2f7a1215fbb4c59dcab50ed125
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-12 16:58:45 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,37 +1,5 @@
-import boto3
-
-def lambda_handler(event, context):
-    if 'findings' in event:
-        security_hub = boto3.client('securityhub')
-
-        finding_types_to_suppress = ['Low']
-        suppression_reason = 'Suppressing low alerts for maintenance window.'
-
-        for finding in event['findings']:
-            severity_label = finding.get('Severity', {}).get('Label', '')
-
-            if severity_label in finding_types_to_suppress:
-                response = security_hub.batch_update_findings(
-                    FindingIdentifiers=[
-                        {
-                            'Id': finding['Id'],
-                            'ProductArn': finding['ProductArn']
-                        }
-                    ],
-                    Workflow=[
-                        {
-                            'Status': 'SUPPRESSED',
-                            'StatusReason': suppression_reason
-                        }
-                    ]
-                )
-
-        return {
-            'statusCode': 200,
-            'body': 'Low alerts suppressed successfully.'
-        }
-    else:
-        return {
-            'statusCode': 400,
-            'body': 'No findings key in the event payload.'
-        }
+Response
+{
+  "statusCode": 400,
+  "body": "No findings key in the event payload."
+}
