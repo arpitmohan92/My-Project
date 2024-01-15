0c5cd4c460a9d17c80aa1254dff977471eeb3053
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-15 20:00:33 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,1 +1,4 @@
-aws s3 ls s3://va-edp-prd-inbound/ALMS/streaming/mde/activity/ --recursive --exclude "*" --include "MDE.MQ.SERVICE.VEL.prod.MDE_ACTIVITY.ID:414d5120514d4831332e5052442020203364946*"
+aws securityhub get-findings --region ap-southeast-2 --profile training-account --output json --filters "SeverityLabel=LOW"
+
+Parameter validation failed:
+Invalid type for parameter Filters.SeverityLabel[0], value: LOW, type: <class 'str'>, valid types: <class 'dict'>
