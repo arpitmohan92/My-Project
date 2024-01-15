309ff7cd9b93482eb3325a94eb0049e4896b344d
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-15 13:14:25 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,1 +1,1 @@
-s3://va-edp-prd-inbound/ALMS/streaming/mde/activity/MDE.MQ.SERVICE.VEL.prod.MDE_ACTIVITY.ID:414d5120514d4831332e5052442020203364946506135965.D2023-03-16T04:41:32.568Z.json
+aws s3 ls s3://va-edp-prd-inbound/ALMS/streaming/mde/activity/ --recursive --exclude "*" --include "MDE.MQ.SERVICE.VEL.prod.MDE_ACTIVITY.ID:414d5120514d4831332e5052442020203364946*"
