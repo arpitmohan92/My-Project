2b3eb3225aafc198d2abc506c4f662c421c426da
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-15 12:12:26 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -6,17 +6,19 @@ AWS_REGION="your-aws-region"
 # Set the suppression reason
 SUPPRESSION_REASON="Suppressing low alerts for maintenance window."
 
-# Get all low severity finding IDs
-LOW_FINDING_IDS=$(aws securityhub get-findings \
+# Get all low severity findings
+LOW_FINDINGS=$(aws securityhub get-findings \
   --region $AWS_REGION \
   --filters "SeverityLabel=Low" \
-  --query 'Findings[*].[Id]' \
   --output json)
 
 # Suppress each low severity finding
-for FINDING_ID in $(echo "${LOW_FINDING_IDS}" | jq -r '.[]'); do
+echo "$LOW_FINDINGS" | jq -c '.Findings[]' | while read -r FINDING; do
+  ID=$(echo "$FINDING" | jq -r '.Id')
+  PRODUCT_ARN=$(echo "$FINDING" | jq -r '.ProductArn')
+
   aws securityhub batch-update-findings \
     --region $AWS_REGION \
-    --finding-identifiers Id=$FINDING_ID \
+    --finding-identifiers Id=$ID,ProductArn=$PRODUCT_ARN \
     --workflow Status=suppressed,StatusReason="$SUPPRESSION_REASON"
 done
