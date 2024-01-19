a7962d5521b889d62ea48033ccd00ffd81b7b061
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-19 13:35:54 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,4 +1,17 @@
-aws securityhub get-findings --region ap-southeast-2 --profile training-account --output json --filters "SeverityLabel=LOW"
+#!/bin/bash
 
-Parameter validation failed:
-Invalid type for parameter Filters.SeverityLabel[0], value: LOW, type: <class 'str'>, valid types: <class 'dict'>
+# Specify your S3 bucket name
+s3_bucket="your-s3-bucket-name"
+
+while IFS= read -r filename; do
+  s3_path="s3://$s3_bucket/$filename"
+  
+  # Check if the file exists in S3
+  if aws s3 ls "$s3_path" &>/dev/null; then
+    # If the file exists, delete it
+    aws s3 rm "$s3_path"
+    echo "Deleted: $filename from $s3_bucket"
+  else
+    echo "File not found in $s3_bucket: $filename"
+  fi
+done < files.txt
