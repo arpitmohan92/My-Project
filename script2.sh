6b51734815aeee09b0381d65768cb45f9cd0d320
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-29 11:16:59 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
-
-# Record start time
-start_time=$(date +"%Y-%m-%d %H:%M:%S")
-echo "Script started at: $start_time" > output.txt
-
-# Specify your S3 bucket name
-bucket_name="your-s3-bucket-name"
-
-# Read file paths from file.txt and delete each file
-while IFS= read -r file_path; do
-    aws s3 rm "s3://$bucket_name/$file_path" >> output.txt 2>&1
-done < file.txt
-
-# Record end time
-end_time=$(date +"%Y-%m-%d %H:%M:%S")
-echo "Script completed at: $end_time" >> output.txt
-
-# Display execution time
-echo "Execution time: $(($(date -d "$end_time" +%s) - $(date -d "$start_time" +%s))) seconds" >> output.txt
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:UpdateService",
+        "ecs:DescribeServices"
+      ],
+      "Resource": "arn:aws:ecs:your-region:your-account-id:service/your-cluster-name/your-service-name",
+      "Condition": {
+        "ArnEquals": {
+          "ecs:cluster": "arn:aws:ecs:your-region:your-account-id:cluster/your-cluster-name"
+        }
+      }
+    }
+  ]
+}
