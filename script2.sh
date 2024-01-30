ef9e28e4907ea454f0a15fa30cbceda32eaa7716
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-01-30 13:47:21 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,97 +1,86 @@
-# IAM Role
+# AWS Lambda Function
+resource "aws_lambda_function" "ecs_service_restart" {
+  function_name = "ECS-Service-Restart"
+  filename      = "lambda_function.zip"  # Specify the path to your Lambda function code
+  runtime       = "python3.8"
+  handler       = "lambda_function.lambda_handler"
+  timeout       = 60
+  role          = aws_iam_role.lambda_execution_role.arn
+}
+
+# IAM Role for Lambda Execution
 resource "aws_iam_role" "lambda_execution_role" {
   name = "lambda_execution_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       }
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
 
-# IAM Policy
+# IAM Policy for Lambda Execution
 resource "aws_iam_policy" "lambda_execution_policy" {
   name        = "lambda_execution_policy"
   description = "Policy for Lambda execution role"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ecs:UpdateService",
-	      "ecs:DescribeServices"
-      ],
-      "Resource": "arn:aws:ecs:ap-southeast-2:254109311541:service/prod-Restricted-Cluster/lms-offer-preferences-service",
-      "Condition": {
-        "ArnEquals": {
-          "ecs:cluster": "arn:aws:ecs:ap-southeast-2:254109311541:cluster/prod-Restricted-Cluster"
-       }	  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ecs:UpdateService",
+          "ecs:DescribeServices"
+        ],
+        Resource = [
+          "arn:aws:ecs:YOUR_REGION:YOUR_ACCOUNT_ID:service/YOUR_CLUSTER/YOUR_SERVICE",
+          # Add more service ARNs if needed
+        ],
+        Condition = {
+          ArnEquals = {
+            "ecs:cluster": "arn:aws:ecs:YOUR_REGION:YOUR_ACCOUNT_ID:cluster/YOUR_CLUSTER"
+          }
+        }
       }
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
 
-# Attach Policy
+# Attach Policy to Role
 resource "aws_iam_role_policy_attachment" "lambda_execution_attachment" {
   policy_arn = aws_iam_policy.lambda_execution_policy.arn
   role       = aws_iam_role.lambda_execution_role.name
 }
 
-#Zip file
-#data "archive_file" "main" {
-#  type        = "zip"
-#  source_file = "./file/lambda_function.py"
-#  output_path = "EBS_Snapshot_Deletion.zip"
-#}
-
-# Lambda Function
-resource "aws_lambda_function" "ecs_service_restart" {
-  function_name = "ECS-service-restart"
-  filename      = "ECS_service_restart.zip"
-#  source_code_hash = data.archive_file.main.output_base64sha256
-  runtime       = "python3.12"
-  handler       = "lambda_function.lambda_handler"
-  timeout       = 600
-  role = aws_iam_role.lambda_execution_role.arn
-}
-
-# EventBridge Rule
+# EventBridge Rule - Trigger Lambda at 7 am and 4 pm daily
 resource "aws_cloudwatch_event_rule" "daily_schedule_rule" {
   name                = "daily_schedule_rule"
   description         = "Rule to trigger Lambda function daily"
-  schedule_expression = "cron(0 7,16 * * ? *)"  
+  schedule_expression = "cron(0 7,16 * * ? *)"  # Schedule to run at 7 am and 4 pm
 
-  event_pattern = <<EOF
-{
-  "source": ["aws.events"],
-  "detail": {
-    "eventName": ["EcsServiceRestartEvent"]
-  }
-}
-EOF
+  event_pattern = jsonencode({
+    source = ["aws.events"],
+    detail = {
+      eventName = ["EcsServiceRestartEvent"]
+    }
+  })
 }
 
 # EventBridge Rule Target to invoke Lambda function
