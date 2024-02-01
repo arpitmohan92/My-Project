e7b5b39c1e1426135d108a1e65528886b6dd706b
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-02-01 15:56:13 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,8 +1,101 @@
-I am trying to identify the source of traffic to the following Trip API endpoint:
+# IAM Role
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda_execution_role"
 
-https://api.vacloud.io/trip/v1/departure/info
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
 
-Specifically I'm looking for the source DNS to try and identify what systems may be calling this endpoint.
+# IAM Policy
+resource "aws_iam_policy" "lambda_execution_policy" {
+  name        = "lambda_execution_policy"
+  description = "Policy for Lambda execution role"
 
-Are you able to retrieve this information via Cloudwatch? I'd need a maximum of 30 days worth of connectivity. If you don't have this much history, whatever you have is fine.
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSnapshotAttribute",
+        "ec2:DescribeSnapshotTierStatus",
+        "ec2:DescribeImages",
+        "ec2:DeleteSnapshot"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
 
+# Attach Policy
+resource "aws_iam_role_policy_attachment" "lambda_execution_attachment" {
+  policy_arn = aws_iam_policy.lambda_execution_policy.arn
+  role       = aws_iam_role.lambda_execution_role.name
+}
+
+#Zip file
+#data "archive_file" "main" {
+#  type        = "zip"
+#  source_file = "./file/lambda_function.py"
+#  output_path = "EBS_Snapshot_Deletion.zip"
+#}
+
+# Lambda Function
+resource "aws_lambda_function" "snapshot_cleanup" {
+  function_name = "Ebs-snapshot-cleanup"
+  filename      = "EBS_Snapshot_Deletion.zip"
+#  source_code_hash = data.archive_file.main.output_base64sha256
+  runtime       = "python3.12"
+  handler       = "lambda_function.lambda_handler"
+  timeout       = 600
+  role = aws_iam_role.lambda_execution_role.arn
+}
+
+# EventBridge Rule
+resource "aws_cloudwatch_event_rule" "daily_schedule_rule" {
+  name                = "daily_schedule_rule"
+  description         = "Rule to trigger Lambda function daily"
+  schedule_expression = "cron(0 0 * * ? *)"  
+
+  event_pattern = <<EOF
+{
+  "source": ["aws.events"],
+  "detail": {
+    "eventName": ["SnapshotCleanupEvent"]
+  }
+}
+EOF
+}
+
+# EventBridge Rule Target to invoke Lambda function
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily_schedule_rule.name
+  target_id = "invoke_lambda_function"
+
+  arn = aws_lambda_function.snapshot_cleanup.arn
+}
