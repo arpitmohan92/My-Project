89e539b02f88288624005157d3ac59650d952a38
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-02-21 17:15:00 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,13 +1,77 @@
-import jenkins.model.Jenkins
+resource "aws_workspaces_directory" "workspaces_directory" {
+  directory_id = var.directory
+  subnet_ids = [var.subnets[0],var.subnets[1]]
+  
 
-def isCLIDisabled() {
-    // Get the Jenkins instance
-    def jenkins = Jenkins.instance
+  self_service_permissions {
+    change_compute_type  = false
+    increase_volume_size = false
+    rebuild_workspace    = true
+    restart_workspace    = true
+    switch_running_mode  = false
+  }
 
-    // Check if the CLI is disabled
-    return jenkins.isCLIOffline()
+  workspace_access_properties {
+    device_type_android    = "ALLOW"
+    device_type_chromeos   = "ALLOW"
+    device_type_ios        = "ALLOW"
+    device_type_linux      = "ALLOW"
+    device_type_osx        = "ALLOW"
+    device_type_web        = "ALLOW"
+    device_type_windows    = "ALLOW"
+    device_type_zeroclient = "DENY"
+  }
+
+  workspace_creation_properties {
+    custom_security_group_id            = aws_security_group.workspace_sg.id
+    default_ou                          = var.default_ou     #"OU=AWS WorkSpaces,OU=UAT,OU=Windows10 20H2,OU=EndUserDevices,DC=virginblue,DC=internal"
+    enable_internet_access              = true
+    enable_maintenance_mode             = true
+    user_enabled_as_local_administrator = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.workspaces_default_service_access,
+    aws_iam_role_policy_attachment.workspaces_default_self_service_access
+  ]
 }
 
-// Example usage
-def cliDisabled = isCLIDisabled()
-println "Jenkins CLI is " + (cliDisabled ? "disabled" : "enabled")
+data "aws_iam_policy_document" "workspaces_iam" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["workspaces.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "workspaces_iam_role" {
+  name               = "workspaces_DefaultRole"
+  assume_role_policy = data.aws_iam_policy_document.workspaces_iam.json
+}
+
+resource "aws_iam_role_policy_attachment" "workspaces_default_service_access" {
+  role       = aws_iam_role.workspaces_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonWorkSpacesServiceAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "workspaces_default_self_service_access" {
+  role       = aws_iam_role.workspaces_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonWorkSpacesSelfServiceAccess"
+}
+
+resource "aws_security_group" "workspace_sg" {
+  name        = "aws-workspaces"
+  description = "Allow outbound traffic from aws workspace"
+  vpc_id      = var.vpc
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
