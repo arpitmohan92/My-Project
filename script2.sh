d57efa3eb3b64eba2e3fe38ffbfec0d4d0df744d
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-03-11 14:48:06 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,155 +1,57 @@
-
-locals {
-  #IAM Roles
-  iam_roles_map_dev = {
-        cc-bayee-sns-push-notification-delivery-cw-log-role = {
-            role_name = "cc-bayee-sns-push-notification-delivery-cw-log-role"
-            role_description = "Role assumed for mobile push notification."
-            custom_role_policy_attachments = [
-                "cc-bayee-sns-push-notification-policy-dev",
-            ]
-
-             custom_trust_policy    = jsonencode(
-{
-	    "Version": "2012-10-17",
-	    "Statement": [
-		{
-			"Effect": "Allow",
-			"Principal": {
-				"Service": "sns.amazonaws.com"
-			},
-			"Action": "sts:AssumeRole"
-		}
-	]
+# Creates a standard S3 bucket
+resource "aws_s3_bucket" "bucket" {
+    bucket      = var.bucket_name
+    tags        = var.tags
 }
-)
-        },                       
+
+resource "aws_s3_bucket_ownership_controls" "acl_disabled" {
+    count  = var.disable_acl ? 1 : 0
+    bucket = aws_s3_bucket.bucket.id
+    rule {
+        object_ownership = "BucketOwnerEnforced"
     }
-    iam_roles_map_test = {}  
-    iam_roles_map      = merge(local.iam_roles_map_dev,local.iam_roles_map_test)  
+}
 
+resource "aws_s3_bucket_ownership_controls" "acl_enabled" {
+    count  = var.disable_acl ? 0 : 1
+    bucket = aws_s3_bucket.bucket.id
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
 
- iam_policies_map_dev = {
-        cc-bayee-sns-push-notification-policy = {
-            name = "cc-bayee-sns-push-notification-policy-dev"
-            path = "/"
-            description = "sns push notification policy"
-            policy = jsonencode(
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents",
-                "logs:PutMetricFilter",
-                "logs:PutRetentionPolicy"
-            ],
-            "Resource": [
-                "*"
-            ]
+resource "aws_s3_bucket_versioning" "bucket" {
+    count  = var.enable_versioning ? 1 : 0
+    bucket = aws_s3_bucket.bucket.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+    bucket = aws_s3_bucket.bucket.id
+    rule {
+        apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
         }
-    ]
-}
-  )
     }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+    bucket                  = aws_s3_bucket.bucket.id
+    restrict_public_buckets = true
+    block_public_policy     = true
+    block_public_acls       = true
+    ignore_public_acls      = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_rule" {
+  bucket      = aws_s3_bucket.bucket.id
+  rule {
+    id     = var.lifecycle_rule_name
+    status = "Enabled"
+    expiration {
+      days = var.lifecycle_object_expire_days
     }
-    iam_policies_map_test = {}
-    iam_policies_map      = merge(local.iam_policies_map_dev,local.iam_policies_map_test)
-
-  iam_roles_map_cloudwatch_dev = {
-       new-relic-cloudwatch-streaming-to-firehose-role= {
-            role_name = "cc-bayee-sns-subs-kinesis-role"
-            role_description = "Role assumed for kinesis stream association"
-            custom_role_policy_attachments = [
-                "new-relic-cloudwatch-streaming-to-firehose",
-            ]
-
- assume_role_policy = jsonencode(
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "streams.metrics.cloudwatch.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole",
-            "Condition": {
-                "StringEquals": {
-                    "sts:ExternalId": "185345276083"
-                }
-            }
-        },
-        {
-            "Sid": "Statement1",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "logs.ap-southeast-2.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole",
-            "Condition": {
-                "StringEquals": {
-                    "sts:ExternalId": "185345276083"
-                }
-            }
-        }
-    ]
+  }
 }
-)
-        },                       
-    }
-    iam_roles_map_cloudwatch_test = {}  
-    iam_roles_map_cloudwatch      = merge(local.iam_roles_map_cloudwatch_dev,local.iam_roles_map_cloudwatch_test) 
-
-iam_policies_map_cloudwatch-dev = {
-        new-relic-cloudwatch-policy = {
-            name = "new-relic-cloudwatch-streaming-to-firehose"
-            path = "/"
-            description = "cloudwatch streaming policy"
-              policy = jsonencode(
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "firehose:PutRecord",
-                "firehose:PutRecordBatch"
-            ],
-            "Effect": "Allow",
-            "Resource": "arn:aws:firehose:ap-southeast-2:185345276083:deliverystream/*",
-            "Sid": "VisualEditor0"
-        }
-    ]
-}
-  )
-    }
-    }
-    iam_policies_map_cloudwatch-test = {}
-    iam_policies_map_cloudwatch       = merge(local.iam_policies_map_cloudwatch-dev,local.iam_policies_map_cloudwatch-test)
-
-
- log_group_map_dev=  {
-    log_group_map={
-    name              = "sns/ap-southeast-2/185345276083/app/GCM/va_app_android"
-    retention_in_days = 120
-}
-
- }
-  log_group_map_test=  {}
-
-    log_group_map_cloudwatch = merge(local.log_group_map_dev,local.log_group_map_test)
-
-cloudwatch_logs_map_subscription_dev = {
-    cloudwatch_logs_subscription_dev= {
-  name = "va_app_android-filter"
-}
-}
-cloudwatch_logs_map_subscription_test = {}
-cloudwatch_logs_map_subscription = merge (local.cloudwatch_logs_map_subscription_dev,local.cloudwatch_logs_map_subscription_test)
-
-}
-
-
