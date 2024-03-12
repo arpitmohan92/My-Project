913f57baa7361c54acfee858b4b2b800ae6cb69e
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-03-13 00:04:45 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,57 +1,26 @@
-# Creates a standard S3 bucket
-resource "aws_s3_bucket" "bucket" {
-    bucket      = var.bucket_name
-    tags        = var.tags
+locals {
+  map_of_s3_buckets_dev = {
+    va-edp-dev-databricks-root-backup = {
+        bucket_name = "va-edp-dev-databricks-root-backup"
+    },
+    va-edp-dev-deep-clone = {
+        bucket_name = "va-edp-dev-deep-clone"
+    },
+...
+
 }
 
-resource "aws_s3_bucket_ownership_controls" "acl_disabled" {
-    count  = var.disable_acl ? 1 : 0
-    bucket = aws_s3_bucket.bucket.id
-    rule {
-        object_ownership = "BucketOwnerEnforced"
+
+main.tf
+
+module "s3-buckets-npr" {
+    source      = "gitlab.com/virginaustralia/aws-s3-bucket/local"
+    version     = "2.0.9"
+    providers   = {aws = aws.edp-service-npr}
+    for_each = {
+         for key, value in local.map_of_s3_buckets : key => value
+          if terraform.workspace == "npr"
     }
-}
-
-resource "aws_s3_bucket_ownership_controls" "acl_enabled" {
-    count  = var.disable_acl ? 0 : 1
-    bucket = aws_s3_bucket.bucket.id
-    rule {
-        object_ownership = "BucketOwnerPreferred"
-    }
-}
-
-resource "aws_s3_bucket_versioning" "bucket" {
-    count  = var.enable_versioning ? 1 : 0
-    bucket = aws_s3_bucket.bucket.id
-    versioning_configuration {
-        status = "Enabled"
-    }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
-    bucket = aws_s3_bucket.bucket.id
-    rule {
-        apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
-        }
-    }
-}
-
-resource "aws_s3_bucket_public_access_block" "bucket" {
-    bucket                  = aws_s3_bucket.bucket.id
-    restrict_public_buckets = true
-    block_public_policy     = true
-    block_public_acls       = true
-    ignore_public_acls      = true
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_rule" {
-  bucket      = aws_s3_bucket.bucket.id
-  rule {
-    id     = var.lifecycle_rule_name
-    status = "Enabled"
-    expiration {
-      days = var.lifecycle_object_expire_days
-    }
-  }
+    bucket      = each.value.bucket_name
+...
 }
