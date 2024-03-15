1b99fc0dced72717044d528f7fab215c86fad078
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-03-15 13:54:03 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -7,7 +7,7 @@ resource "aws_iam_role" "backup_role" {
         {
           "Effect" : "Allow"
           "Principal" : {
-            "Service" : var.trusted_aws_services
+            "Service" : "cloudformation.amazonaws.com"
           },
           "Action" : "sts:AssumeRole"
         }
@@ -21,4 +21,20 @@ resource "aws_iam_role_policy_attachment" "policy_attach" {
   count      = length(var.role_policy_arns)
   role       = aws_iam_role.backup_role.name
   policy_arn = element(var.role_policy_arns, count.index)
+}
+
+module "ts_sizzl_iam" {
+  count         = terraform.workspace == "npr" ? 1 : 0
+  source        = "./Modules/IAM/"
+  iam_role_name = "aws-backup-reporter-cloudformation-role"
+  tags = local.common_tags
+  providers         = {aws = aws.ts-sizzl-backup-prd}
+  role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonAthenaFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess",
+    "arn:aws:iam::aws:policy/AWSLambda_FullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess"
+  ]
 }
