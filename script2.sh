1638358ea9ac5b7f9c0a51cd857c721d9994a4c9
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-03-19 14:35:05 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,15 +1,15 @@
-apiVersion: batch/v1beta1
-kind: CronJob
-metadata:
-  name: example-cronjob
-spec:
-  schedule: "*/1 * * * *"  # Schedule to run every minute
-  jobTemplate:
-    spec:
-      template:
-        spec:
-          containers:
-          - name: job-container
-            image: busybox
-            command: ["echo", "Hello from the CronJob"]
-          restartPolicy: OnFailure
+resource "aws_s3_bucket_ownership_controls" "acl_disabled" {
+    count  = var.disable_acl ? 1 : 0
+    bucket = aws_s3_bucket.bucket.id
+    rule {
+        object_ownership = "BucketOwnerEnforced"
+    }
+}
+
+resource "aws_s3_bucket_ownership_controls" "acl_enabled" {
+    count  = var.disable_acl ? 0 : 1
+    bucket = aws_s3_bucket.bucket.id
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
