3092dff810aec0a981da435c89425490e5badf68
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-04-04 19:53:17 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,15 +1,2 @@
-resource "aws_s3_bucket_ownership_controls" "acl_disabled" {
-    count  = var.disable_acl ? 1 : 0
-    bucket = aws_s3_bucket.bucket.id
-    rule {
-        object_ownership = "BucketOwnerEnforced"
-    }
-}
-
-resource "aws_s3_bucket_ownership_controls" "acl_enabled" {
-    count  = var.disable_acl ? 0 : 1
-    bucket = aws_s3_bucket.bucket.id
-    rule {
-        object_ownership = "BucketOwnerPreferred"
-    }
-}
+    - set_fact:
+        application_version: "{% if dev_deploy_snapshot == true %}"{{ app_version }}"-SNAPSHOT{% else %}{{ app_version }}{% endif %}"
