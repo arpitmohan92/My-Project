9e440414f6a327664a98311ee6a9a814f6b43815
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2024-04-16 13:18:05 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,1 +1,18 @@
+Plan Resource Requirements: Assess the computing power, storage, and networking needs before launching an instance.
+Follow Naming Conventions: Adhere to naming conventions for easy identification and management of resources.
+Implement Security Groups: Define appropriate security groups to restrict access to necessary ports and protocols.
+Apply Tags: Tag instances with relevant metadata such as owner, purpose, and environment for better organization and cost tracking.
+Enable Monitoring: Enable CloudWatch monitoring for the instance to monitor performance and health.
+Regular Updates: Keep the instance updated with the latest security patches and software updates.
+Back Up Data: Implement regular backups of important data stored on the instance to prevent data loss.
+Implement IAM Roles: Assign IAM roles with the least privilege principle to instances to restrict access to AWS resources.
+Don'ts:
 
+Ignore Security Best Practices: Avoid launching instances without implementing necessary security measures like encryption and access controls.
+Use Default Settings Blindly: Don't rely on default settings; customize configurations based on organizational requirements.
+Forget to Terminate Unused Instances: Avoid leaving instances running when they are no longer needed to save costs.
+Share Key Pairs: Don't share key pairs used to access instances with unauthorized individuals.
+Ignore Compliance Requirements: Ensure compliance with organizational and regulatory requirements when configuring instances.
+Overprovision Resources: Avoid overprovisioning resources by selecting instance types that match workload requirements.
+Store Sensitive Data Without Encryption: Don't store sensitive data on instances without proper encryption measures in place.
+Neglect Monitoring: Don't neglect monitoring and managing instances after launch; regularly review performance and security.
